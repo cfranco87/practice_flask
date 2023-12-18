124063ddf5206f714eba5540d71ac73cb26a1598
root <carlosfrancoart@gmail.com>
2023-12-18 17:06:54 +0000

    flask v10

--- website/__init__.py
+++ website/__init__.py
@@ -6,6 +6,10 @@ def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'the yellow pages'
     """secret_key adds security to site"""
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    """app.config is telling where the database is located"""
+    db.init_app(app)
+    """enables you ti use SQL within Flask app to work with databases"""
 
     """registering view, auth"""
     from .views import views
--- website/auth.py
+++ website/auth.py
@@ -1,5 +1,12 @@
 from flask import Blueprint, render_template
-"""render_template will call files named, ex login.html""" 
+"""render_template will call files named, ex login.html"""
+from flask_sqlalchemy import SQLAlchemy
+"""Flask_sqlalchemy module that can use databases"""
+
+"""db  instance will be used to interact w database in flask"""
+db = SQLAlchemy()
+"""variable DB_NAME to store the name of the database file, database.db"""
+DB_NAME = "database.db"
 
 auth = Blueprint('auth', __name__)
 
@@ -13,4 +20,25 @@ def logout():
 
 @auth.route('/sign-up')
 def sign_up():
+    """POST REQUEST asks for info, email, name, passwordsin form"""
+    if request.method == 'POST':
+        email = request.form.get('email')
+        first_name = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+    
+    """if POST doesnt meet criteria then it will flash error, using flash module"""
+    if user:
+            flash('Email already exists.', category='error')
+        elif len(email) < 4:
+            flash('Email must be greater than 3 characters.', category='error')
+        elif len(first_name) < 2:
+            flash('First name must be greater than 1 character.', category='error')
+        elif password1 != password2:
+            flash('Passwords don\'t match.', category='error')
+        elif len(password1) < 7:
+            flash('Password must be at least 7 characters.', category='error')
+        else:
+            flash('')
+
     return render_template("signup.html")
